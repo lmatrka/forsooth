795a1de3db5d5bfcad06b92a261bdcfd37cd25fd
lmaturka <lauren.matrka@gmail.com>
2021-09-29 03:20:43 -0400

    added styling and firebase db

--- scratch1.js
+++ scratch1.js
@@ -1,22 +1,68 @@
 //Enter a Messenger, with two heads and a hand
+const firebaseConfig = {
+  apiKey: 'AIzaSyDQ3-rZzCs-LwixdPDzitmOBkGdiqjLDCg',
+  authDomain: 'forsooth-27fe4.firebaseapp.com',
+  databaseURL: 'https://forsooth-27fe4-default-rtdb.firebaseio.com/',
+  projectId: 'forsooth-27fe4',
+  storageBucket: 'forsooth-27fe4.appspot.com',
+  messagingSenderId: '558006587830',
+  appId: '1:558006587830:web:727b8c4bb89a47f1873117'
+};
+firebase.initializeApp(firebaseConfig);
+const db = firebase.database();
 
 const plays = [];
 const playerScenes = [];
 const roleElements = [];
-let assignedRoleLists;
 let currentPlay;
 let playerCount = 0;
+let assignedRoleLists;
 let selectedRole;
 let playerButtons;
-//let lines = new Array(5).fill(0).map(() => new Array(15));
+
 const playList = document.getElementById('playList');
-const submitButton = document.getElementById('submit');
 const playTitle = document.querySelector('#playTitle h1');
 const playerList = document.getElementById('playerList');
 const roleList = document.getElementById('characterList');
+document.getElementById('about').addEventListener('click', function(){});
+document.getElementById('reset').addEventListener('click', resetLower );
+
+document.getElementById('load').addEventListener('click', function(){});
+
+(function() {
+  document.getElementById('save').addEventListener('click', function(){
+    const username = document.getElementById('username').value;
+    if(username !== '' && currentPlay !== undefined){
+      const progressRef = db.ref('savedPlays');
+      progressRef.push({
+        u: username,
+        cP: currentPlay,
+        pC: playerCount,
+        rE: roleElements
+      });
+    }
+  });
+  document.getElementById('load').addEventListener('click', function(){
+    const username = document.getElementById('username').value;
+    db.ref('savedPlays').on('value', function(results) {
+      const allSavedPlays = results.val();
+      for (let savedPlay in allSavedPlays) {
+        if(allSavedPlays[savedPlay].u === username){
+          loadSavedPlay(allSavedPlays[savedPlay]);
+          break;
+        }
+      }
+    });
+  });
+})();
 
 
-fetch("https://gist.githubusercontent.com/lmatrka/a78299e39648e7185344203d999f8e91/raw/094e11c43a7c37134cadc6f403ad5e74487315ab/shakespeare.json")
+document.getElementById('submit').addEventListener('click', sortPlays );
+
+
+
+
+fetch('https://gist.githubusercontent.com/lmatrka/a78299e39648e7185344203d999f8e91/raw/094e11c43a7c37134cadc6f403ad5e74487315ab/shakespeare.json')
   .then((response) => {
     if (response.ok) {
       return response.json();
@@ -35,6 +81,8 @@ fetch("https://gist.githubusercontent.com/lmatrka/a78299e39648e7185344203d999f8e
     });
     const uniquePlayTitles = [...new Set(allPlayTitles)];
     
+    ////////////////////////////////////////////////////////////////// create play object ///////////////
+    
     uniquePlayTitles.forEach(play => {
       const playObj = {
         title: play,
@@ -44,52 +92,52 @@ fetch("https://gist.githubusercontent.com/lmatrka/a78299e39648e7185344203d999f8e
           all: [],
           major: [],
           minor: []
-        },
-        fullyLoaded: false
+        }
       };
+
+      indexScenes(playObj);
       playObj.lines.forEach(line => {
-        if(line.speaker !== ""){
+        if(line.speaker !== ''){
           if(!playObj.characters.all.includes(line.speaker)){
             playObj.characters.all.push(line.speaker);
           }
         }
       });
+
       playObj.characters.all.shift(); /*act and scene entries list the last speaker in line.speaker, so each play starts with a speaker from the previous play*/
-      playObj.characters.all.forEach(character => {
-        if(isUpper(character)){
-          playObj.characters.major.push(character);
+      playObj.characters.all = playObj.characters.all.map(x => [x]);
+      characterDetails(playObj);
+      playObj.characters.all.forEach(charArray => {
+        if(charArray[2] > 50){
+          playObj.characters.major.push(charArray[0]);
         }else{
-          playObj.characters.minor.push(character);
+          playObj.characters.minor.push(charArray[0]);
         }
       });
-      //playObj.characters.filter((item, index) => playObj.characters.indexOf(item) === index);
+
       plays.push(playObj);
     });
 
+    /////////////////////////////////////////////////////////////////////////////////////////////////////
+    
     plays.forEach(play => {
       const title = document.createElement('li');
-      title.innerHTML = `${play.title} - ${play.characters.all.length} characters (${play.characters.major.length}/${play.characters.minor.length})`;
+      title.innerHTML = `<strong>${play.title}</strong>  <span>${play.characters.all.length} characters (${play.characters.major.length} major/ ${play.characters.minor.length} minor)</span>`;
       title.classList.add('unavail');
       playList.appendChild(title);
     });
   });
 
-
-function isUpper(str) {
-  return !/[a-z]/.test(str) && /[A-Z]/.test(str);
-}
-
-submitButton.addEventListener('click', sortPlays );
-
+ /////////////////////////////////////////////////////////////////////////////////////////////////////
 function sortPlays(){
-  playList.innerHTML = "";
+  playList.innerHTML = '';
   playerCount = parseInt(document.getElementById('players').value);
   const single = document.getElementById('single');
   const minor = document.getElementById('minor');
   const multiple = document.getElementById('multiple');
   plays.forEach(play => {
     const title = document.createElement('li');
-    title.innerHTML = `${play.title} - ${play.characters.all.length} characters (${play.characters.major.length}/${play.characters.minor.length})`;
+    title.innerHTML = `<strong>${play.title}</strong>  <span>${play.characters.all.length} characters (${play.characters.major.length} major/ ${play.characters.minor.length} minor)</span>`;
     if(single.checked && play.characters.all.length === playerCount){
       playList.appendChild(title);
       title.addEventListener('click', function(){loadPlay(play)} );
@@ -103,23 +151,10 @@ function sortPlays(){
   });
 }
 
-function loadPlay(play){
-  currentPlay = play;
-  playTitle.innerText = currentPlay.title;
-  loadPlayers(playerCount);
-  if (currentPlay.fullyLoaded === false){
-    indexScenes(currentPlay);
-    currentPlay.characters.all = play.characters.all.map(x => [x]);
-    currentPlay.characters.all.forEach(charArray => {loadCharacter(charArray)});
-    currentPlay.fullyLoaded = true;
-  }
-  loadRoles(currentPlay);
-}
-
 function indexScenes(play) {
   let previous = 0;
   play.lines.forEach((line, index) => {
-    if(line.line_number !== ""){
+    if(line.line_number !== ''){
       const lineNumber = line.line_number.split('.');
       if(lineNumber[1] !== previous){
         play.sceneIndex.push(index);
@@ -130,28 +165,38 @@ function indexScenes(play) {
   play.sceneIndex.push(play.lines.length-1);
 }
 
-function loadCharacter(charArray){
-  let activeScenes = [];
-  let numLines = 0;
-  //let sceneCounter = 0;
-  for(let i = 0; i < currentPlay.sceneIndex.length; i++){
-    //sceneCounter++;
-    for(let j = currentPlay.sceneIndex[i]; j < currentPlay.sceneIndex[i+1]; j++){
-      if(currentPlay.lines[j].speaker === charArray[0]){
-        numLines++;
-        activeScenes.push(i+1);
+function characterDetails(play){
+  play.characters.all.forEach(charArray => {
+    let activeScenes = [];
+    let numLines = 0;
+    for(let i = 0; i < play.sceneIndex.length; i++){
+      for(let j = play.sceneIndex[i]; j < play.sceneIndex[i+1]; j++){
+        if(play.lines[j].speaker === charArray[0]){
+          numLines++;
+          activeScenes.push(i+1);
+        }
       }
     }
-  }
-  activeScenes = activeScenes.filter((v, i, a) => a.indexOf(v) === i);
-  charArray.push(activeScenes, numLines);
-  //console.log(charArray);
+    activeScenes = activeScenes.filter((v, i, a) => a.indexOf(v) === i);
+    charArray.push(activeScenes, numLines);
+  });
+}
+ /////////////////////////////////////////////////////////////////////////////////////////////////////
+
+function loadPlay(play){
+  playerList.innerHTML = '<h3>Players</h3>';
+  reset();
+  currentPlay = play;
+  playTitle.innerText = currentPlay.title;
+  document.getElementById('lower').style.visibility = 'visible';
+  loadPlayers(playerCount);
+  loadRoles(currentPlay);
 }
 
 function loadPlayers(num){
   for(let i=0; i < num; i++){
     const playerDiv = document.createElement('div');
-    playerDiv.innerHTML = `<input type="text" id="player" placeholder="player ${i+1}"><span><strong> +</strong></span><br>
+    playerDiv.innerHTML = `<input type="text" id="player" placeholder="player ${i+1}"><span style="font-size:24px"><strong> +</strong></span><br>
     <ul id="#player${i}" class="assignedRoles"></ul>`;
     playerList.appendChild(playerDiv);
     playerScenes.push([]);
@@ -163,25 +208,25 @@ function loadRoles(play){
   play.characters.all.forEach((charArray, index) => {
     const role = document.createElement('li');
     role.innerHTML = `<strong>${charArray[0]},</strong> ${charArray[2]} lines`;
-    role.addEventListener('click', function(){checkPlayers(index)} );
+    role.addEventListener('click', function(){checkPlayers(index)});
     roleElements.push([role, 'X']);
     roleList.appendChild(role);
     console.log(charArray);
   });
 }
 
+ /////////////////////////////////////////////////////////////////////////////////////////////////////
+
 function checkPlayers(index){
   selectedRole = index;
-  playerButtons = document.querySelectorAll('span');
+  playerButtons = document.querySelectorAll('#lower span');
   playerButtons.forEach((button, i) => {
     button.style.color = 'black';
     if(!checkScenes(selectedRole, i)){
-
       button.style.color = 'red';
       button.addEventListener('click', function(){
         roleElements[selectedRole][1]=i; 
         sortRoles();
-        currentPlay.characters.all[selectedRole][1].forEach(scene => {playerScenes[i].push(scene)});
         this.removeEventListener('click', arguments.callee);
       });
     }
@@ -189,23 +234,63 @@ function checkPlayers(index){
 }
 
 function sortRoles(){
-  roleList.innerHTML = "";
-  assignedRoleLists.forEach(list => {
-    list.innerHTML = "";
-  });
-  roleElements.forEach(role => {
+  roleList.innerHTML = '';
+  assignedRoleLists.forEach(list => list.innerHTML = '');
+  playerScenes.forEach(array => array.length = 0);
+  roleElements.forEach((role, index) => {
     if(role[1] === 'X'){
       roleList.appendChild(role[0]);
     }else{
       assignedRoleLists[role[1]].appendChild(role[0]);
+      currentPlay.characters.all[index][1].forEach(scene => playerScenes[role[1]].push(scene));
     }
   });
   playerButtons = document.querySelectorAll('span');
-  playerButtons.forEach(button => {button.style.color = 'black'});
+  playerButtons.forEach(button => button.style.color = 'black');
 }
 
 function checkScenes(role, player){
   return playerScenes[player].some(r=> currentPlay.characters.all[role][1].includes(r));
+}
+
+ /////////////////////////////////////////////////////////////////////////////////////////////////////
+
+function loadSavedPlay(savedPlay){
+  playerList.innerHTML = '<h3>Players</h3>';
+  reset();
+  currentPlay = savedPlay.cP;
+  playTitle.innerText = currentPlay.title;
+  document.getElementById('lower').style.visibility = 'visible';
+  loadSavedRoles(savedPlay.rE);
+  loadPlayers(savedPlay.pC);
+  sortRoles();
+}
+
+function loadSavedRoles(savedRoles){
+  currentPlay.characters.all.forEach((charArray, index) => {
+    const role = document.createElement('li');
+    role.innerHTML = `<strong>${charArray[0]},</strong> ${charArray[2]} lines`;
+    role.addEventListener('click', function(){checkPlayers(index)});
+    roleElements.push([role, savedRoles[index][1]]);
+  });
+}
+
+function resetLower(){
+  if (confirm('Are you sure you want to clear this play?')){
+    document.getElementById('lower').style.visibility = 'hidden';
+    playTitle.innerText = '';
+    currentPlay = undefined;
+  }
+}
+
+function reset() {
+  roleList.innerHTML = '';
+  playerScenes.length = 0;
+  roleElements.length = 0;
+  assignedRoleLists = 0;
+  selectedRole = 0;
+  playerButtons = 0;
+  console.log('reset');
 }
 
 
